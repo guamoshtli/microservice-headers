c3ab025f704993762c9bef4942d56369463c8c29
Gonzalo Sandoval <gsandoval@amucss.net>
2016-02-03 16:31:24 -0600

    ip change in index.js 2

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
 			var h=request.headers
 			var lang = h["accept-language"].split(',')[0];
 			var soft = h["user-agent"].split(') ')[0].split(' (')[1] 
-      var ip = h.['x-forwarded-for'] || 
+      var ip = h['x-forwarded-for'] || 
      request.connection.remoteAddress || 
      request.socket.remoteAddress ||
      request.connection.socket.remoteAddress;
